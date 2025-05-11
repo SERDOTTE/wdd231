dfb3000cfa96cee699f7dee5a4814b59a20cefcc
serdotte <rodrigo.serdotte@gmail.com>
2025-05-11 16:57:23 -0300

    inicial estructure

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById("copyrightYear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
